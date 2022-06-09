6b1164a1b76d3d977cfe54061b0ae6ed5ca4aee7
Manuel Romero <65180104+Hazdan@users.noreply.github.com>
2022-06-09 10:40:19 -0500

    Quarter of the year

---
+++ quarter_of_the_year.go
@@ -0,0 +1,20 @@
+package kata
+
+func QuarterOf(month int) int {
+  months := map[int]int{
+    1: 1,
+    2: 1,
+    3: 1,
+    4: 2,
+    5: 2,
+    6: 2,
+    7: 3,
+    8: 3,
+    9: 3,
+    10: 4,
+    11: 4,
+    12: 4,
+  }
+  
+  return months[month]
+}
