1df5db09e14d915bb498c77605f101b5c226c5c2
Manuel Romero <65180104+Hazdan@users.noreply.github.com>
2022-06-10 17:08:52 -0500

    Upgrade Bit Counting

--- bit_counting.go
+++ bit_counting.go
@@ -1,10 +1,16 @@
 package kata
 
-import (
-  "fmt"
-  "strings"
-)
+// import (
+//   "fmt"
+//   "strings"
+// )
+
+// func CountBits(n uint) int {
+//   return len(strings.Replace(fmt.Sprintf("%b", n), "0", "", -1))
+// }
+
+import "math/bits"
 
 func CountBits(n uint) int {
-  return len(strings.Replace(fmt.Sprintf("%b", n), "0", "", -1))
+  return bits.OnesCount(n)
 }
