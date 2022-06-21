68c63e88af3656025dfca0fb0a8374c58a17c0c7
Manuel Romero <65180104+Hazdan@users.noreply.github.com>
2022-06-21 17:15:28 -0500

    Make a function that does arithmetic

---
+++ make_a_function_that_does_arithmetic.go
@@ -0,0 +1,15 @@
+package kata
+
+func Arithmetic(a int, b int, operator string) int{
+  switch operator {
+    case "add":
+      return a + b
+    case "subtract":
+      return a - b
+    case "multiply":
+      return a * b
+    case "divide":
+      return a / b
+  }
+  return 0
+}
